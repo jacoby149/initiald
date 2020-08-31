9af6377336db98f3333e6147954b52312aa6b0c2
jacoby149 <jackstudent149@gmail.com>
2020-08-31 17:08:06 -0400

    initial d working for me

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 import setuptools
 from setuptools import setup
 
-import initiald
-
-initiald.create_inits()
-
-setup(name='tlibs',
+setup(name='initiald',
       version='2.0',
       description='homemade pipe bomb shit',
-      author='the affluent jew',
+      author='the ghoozie and his friend the affluent jew',
       author_email="you don't need to know this",
       url='i said it was homemade bishhh',
       packages=setuptools.find_packages()
